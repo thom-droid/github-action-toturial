7651aaf515340c0ca8887b6cc77b7cd4817d44d4
thom-window <hsbang.thom@gmail.com>
2022-08-08 16:25:02 +0900

    initial commit

---
+++ src/test/java/com/codestates/githubactionstutorial/GithubActionsTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codestates.githubactionstutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubActionsTutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
